d6ba256bc823453b1a77a8d4a992f7b8e595e7bf
datanalystmgm <analisdatamgm@gmail.com>
2026-03-01 14:59:34 +0700

    b

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { 
   getFirestore, 
   collection, 
@@ -29,7 +28,6 @@ const firebaseConfig = {
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export default function InventoryPage() {
